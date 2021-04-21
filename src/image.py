1b4e418df3683e896b09609097a3a8b681cd2a77
Christian de Jonge <chjo@equinor.com>
2021-04-21 14:00:29 +0200

    Restructure and code and find green ziptie on main run

---
+++ src/image.py
@@ -0,0 +1,60 @@
+import numpy as np
+import cv2
+
+
+def get_image_from_bytes_string(bytes):
+    nparr = np.frombuffer(bytes, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return image
+
+
+def find_color_in_image(image, lower_color_bound, upper_color_bound):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    image = cv2.inRange(image, lower_color_bound, upper_color_bound)
+    return image
+
+
+def get_biggest_contour(binary_image):
+    cont, _ = cv2.findContours(binary_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    biggest_contour = []
+    biggest_area = 0
+    for con in cont:
+        area = cv2.contourArea(con)
+        if area > biggest_area:
+            biggest_area = area
+            biggest_contour = con
+    return biggest_contour
+
+
+def draw_bounding_box(image, bounding_box_xywh, text, color):
+    x, y, w, h = bounding_box_xywh
+
+    pt1 = (x, y)
+    pt2 = (x + w, y + h)
+    cv2.rectangle(image, pt1, pt2, color, thickness=3)
+
+    font_size = 0.8
+    cv2.putText(
+        image,
+        text,
+        (x, y),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        font_size,
+        color,
+        2,
+        cv2.LINE_AA,
+    )
+
+
+def show_image(image):
+    cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+    cv2.imshow("image", image)
+    while cv2.getWindowProperty("image", cv2.WND_PROP_VISIBLE) > 0:
+        key = cv2.waitKey(100)
+        ESCAPE_KEY_CODE = 27
+        if key == ESCAPE_KEY_CODE:
+            break
+        else:
+            continue
+
+    cv2.destroyAllWindows()
--- src/main.py
+++ src/main.py
@@ -1,34 +1,35 @@
-from azure.storage.blob import ContainerClient
 import cv2
-import time
 import numpy as np
+from azure.storage.blob import ContainerClient
 
 from config import STORAGE_CONNECTION_STRING, STORAGE_CONTAINER_NAME
+from image import (
+    draw_bounding_box,
+    find_color_in_image,
+    get_biggest_contour,
+    get_image_from_bytes_string,
+    show_image,
+)
 
+lower_green_bound = np.array([65, 50, 50])
+upper_green_bound = np.array([87, 255, 255])
 
 if __name__ == "__main__":
     container_client = ContainerClient.from_connection_string(
-        STORAGE_CONNECTION_STRING,
-        container_name=STORAGE_CONTAINER_NAME
+        STORAGE_CONNECTION_STRING, container_name=STORAGE_CONTAINER_NAME
     )
     blob = container_client.get_blob_client("images/green1.jpg")
     blob_downloader = blob.download_blob()
-
     bytes = blob_downloader.readall()
+    image = get_image_from_bytes_string(bytes)
 
-    nparr = np.frombuffer(bytes, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    image_binary_where_green = find_color_in_image(
+        image, lower_green_bound, upper_green_bound
+    )
+    biggest_contour = get_biggest_contour(image_binary_where_green)
+    bounding_box_xywh = cv2.boundingRect(biggest_contour)
+    draw_bounding_box(image, bounding_box_xywh, "green ziptie", [0, 255, 0])
 
-    cv2.namedWindow("image", cv2.WINDOW_NORMAL)
-    cv2.imshow("image", image)
-    
-    while True:
-        key = cv2.waitKey(0)
-        ESCAPE_KEY_CODE = 27
-        if key == ESCAPE_KEY_CODE:
-            break
-        else:
-            time.sleep(0.1)
-            continue
-
-    cv2.destroyAllWindows()
+    image_2 = cv2.cvtColor(image_binary_where_green, cv2.COLOR_GRAY2BGR)
+    image_to_show = np.concatenate((image, image_2), axis=1)
+    show_image(image_to_show)
