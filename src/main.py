9773e2f188469786ee9f54d47f3debf0eb88847d
Nicholas Dalhaug <NICD@equinor.com>
2021-04-22 15:10:56 +0200

    Add cmd arguments

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from azure.storage.blob import ContainerClient
+import argparse
 
 from config import STORAGE_CONNECTION_STRING, STORAGE_CONTAINER_NAME
 from image import (
@@ -14,7 +15,39 @@ from image import (
 lower_green_bound = np.array([65, 50, 50])
 upper_green_bound = np.array([87, 255, 255])
 
-if __name__ == "__main__":
+
+def main():
+    color_name, medium, should_upload = get_arguments()
+    show_one_detected_image()
+
+
+def get_arguments():
+    args = build_arg_parser()
+    color_name = args.color
+    medium = args.medium
+    should_upload = args.upload
+
+    return color_name, medium, should_upload
+
+
+def build_arg_parser():
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        "--color", help="color of ziptie detected (red | green)", required=True
+    )
+    ap.add_argument(
+        "--medium", help="what medium to detect in (image | video)", required=True
+    )
+    ap.add_argument(
+        "--upload",
+        help="flag for whether or not do uplaod result (default: False)",
+        action="store_true",
+    )
+
+    return ap.parse_args()
+
+
+def show_one_detected_image():
     container_client = ContainerClient.from_connection_string(
         STORAGE_CONNECTION_STRING, container_name=STORAGE_CONTAINER_NAME
     )
@@ -33,3 +66,7 @@ if __name__ == "__main__":
     image_2 = cv2.cvtColor(image_binary_where_green, cv2.COLOR_GRAY2BGR)
     image_to_show = np.concatenate((image, image_2), axis=1)
     show_image(image_to_show)
+
+
+if __name__ == "__main__":
+    main()
