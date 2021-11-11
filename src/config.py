c0d78477002e366402bce79ede60d07b4814168e
Thomas Sundvoll <thomas@sundvoll.net>
2021-11-11 16:13:59 +0100

    Fix always use double quotation marks

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-STORAGE_CONNECTION_STRING = os.getenv('STORAGE_CONNECTION_STRING')
+STORAGE_CONNECTION_STRING = os.getenv("STORAGE_CONNECTION_STRING")
 STORAGE_CONTAINER_NAME = "testdata"
